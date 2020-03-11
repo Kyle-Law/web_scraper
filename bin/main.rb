d70435f065f3898b7e9f02d184371a3e34429e06
kyle-law <junwei_1121@hotmail.com>
2020-03-11 18:40:03 +0800

    Added remote.io Scraper

--- bin/main.rb
+++ bin/main.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/env ruby
 
-require_relative '../lib/Udacity_Scraper.rb'
-require_relative '../lib/Indeed_Scraper.rb'
+require_relative '../lib/Scraper.rb'
 
-puts "Which website do you want to scrap? (udacity / indeed)"
-website = (gets.chomp + "_scraper").to_sym
+puts "Which website do you want to scrap? (udacity / indeed / remote.io)"
+website = (gets.chomp.gsub(".","") + "_scraper").to_sym
 
 scrap = Scraper.new
 scrap.send(website)
--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -7,7 +7,7 @@ class Scraper
     def udacity_scraper
         url = 'https://www.udacity.com/courses/all'
         unparsed_page = HTTParty.get(url)
-        parsed_page = Nokogiri::HTML(unparsed_page)
+        parsed_page = Nokogiri::HTML(unparsed_page.body)
         courses = []
         course_listings = parsed_page.css('div.card-content')
         course_listings.each do |listing|
@@ -19,19 +19,20 @@ class Scraper
             }
         end
         File.write("udacity_courses.txt",courses.join("\n"))
+        puts "udacity_courses.txt file is created at the root directory with #{courses.length} udacity courses"
     end
 
     def indeed_scraper
         url = "https://www.indeed.com/jobs?q=Remote+Web+Developer&rbl=Remote&jlid=aaa2b906602aa8f5&explvl=entry_level"
         unparsed_page = HTTParty.get(url)
-        parsed_page = Nokogiri::HTML(unparsed_page)
+        parsed_page = Nokogiri::HTML(unparsed_page.body)
         total_pages = parsed_page.css('div.searchCount-a11y-contrast-color').text[48..50].to_i
         page_start_num = (0..total_pages).to_a.select{|i| i%10==0}
         jobs = []
         page_start_num.each do |start|
             url = "https://www.indeed.com/jobs?q=Remote+Web+Developer&rbl=Remote&jlid=aaa2b906602aa8f5&explvl=entry_level&start=#{start}"
             unparsed_page = HTTParty.get(url)
-            parsed_page = Nokogiri::HTML(unparsed_page)
+            parsed_page = Nokogiri::HTML(unparsed_page.body)
             jobs_listings = parsed_page.css('div.jobsearch-SerpJobCard')
             jobs_listings.each do |listing|
                 if listing.css('span.date').text != "Today"
@@ -48,15 +49,48 @@ class Scraper
                     company: listing.css('span.company').text,
                     location: listing.css('span.location').text,
                     summary: listing.css('div.summary').text.gsub("\n","-"),
-                    day_posted: "0"
+                    day_posted: "0 day ago"
                     }
                 end
             end
+            puts "#{jobs.length} remote jobs have been scraped..."
         end
         # courses.each{|i| puts i}
         sorted = jobs.sort_by{|hash| hash[:day_posted]}
         # byebug
-        File.write("indeed_job.txt",sorted.join("\n"))
+        File.write("indeed_jobs.txt",sorted.join("\n"))
+        puts "indeed_jobs.txt file is created at the root directory with #{jobs.length} jobs."
+    end
+
+    def remoteio_scraper
+        url = "https://www.remote.io/remote-jobs?s=ruby-on-rails&p=1"
+        unparsed_page = HTTParty.get(url)
+        parsed_page = Nokogiri::HTML(unparsed_page.body)
+        # byebug
+        total_pages = ((parsed_page.css('small').text[1,3].to_f) % 20).round
+        page_start_num = (1..total_pages).to_a
+        jobs = []
+        page_start_num.each do |page|
+            url = "https://www.remote.io/remote-jobs?s=ruby-on-rails&p=#{page}"
+            unparsed_page = HTTParty.get(url)
+            parsed_page = Nokogiri::HTML(unparsed_page.body)
+            jobs_listings = parsed_page.css('div.job-listing-description')
+            jobs_listings.each do |listing|
+                jobs << {
+                    title: listing.css('h3.job-listing-title').text,
+                    company: listing.css('div.job-listing-footer').text.split("  ")[2],
+                    skills: listing.css('div.job-listing-footer').text.split("  ")[4],
+                    day_posted: listing.css('div.job-listing-footer').text.split("  ")[3]
+                }
+            end
+            puts "parsed page #{page}"
+        end
+        # byebug
+        # courses.each{|i| puts i}
+        sorted = jobs.sort_by{|hash| hash[:day_posted][0,4].to_i}
+        # byebug
+        File.write("remote_io.txt",sorted.join("\n")) unless sorted.empty?
+    
     end
 end
 
